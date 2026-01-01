0a90213f4de109f7e0b88162391e1a92741d949e
respectbeko162 <salih.demirci@stu.fbu.edu.tr>
2026-01-01 18:59:01 +0300

    Week 3: PostgreSQL schema initialization and C++ query execution

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <postgresql/libpq-fe.h>
 
 int main() {
-    std::cout << "Student Information System\n";
+    const char* conninfo =
+   	 "host=postgres "
+   	 "port=5432 "
+   	 "dbname=sisdb "
+   	 "user=sisuser "
+   	 "password=sispass";
+
+
+    PGconn* conn = PQconnectdb(conninfo);
+
+    if (PQstatus(conn) == CONNECTION_OK) {
+        std::cout << "Connected to database successfully" << std::endl;
+    } else {
+        std::cerr << "Connection failed: "
+                  << PQerrorMessage(conn) << std::endl;
+        PQfinish(conn);
+        return 1;
+    }
+
+    PGresult* res = PQexec(conn, "SELECT 1;");
+    if (PQresultStatus(res) == PGRES_TUPLES_OK) {
+        std::cout << "Query works!" << std::endl;
+    }
+
+    PQclear(res);
+    PQfinish(conn);
     return 0;
 }
