d01632a3a3266488a173e710419266f4d9217c4c
ramezak123-ctrl <ramez.ak123@gmail.com>
2026-01-13 21:05:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
+#include <iostream>
 #include "student_repository.hpp"
 
 int main() {
     StudentRepository repo;
-    repo.add_student();
+
+    repo.add_student("Ali");
+    repo.add_student("Omar");
+
+    repo.read_students();
+
+    repo.update_student(1, "Ahmed");
+    repo.delete_student(2);
+
     repo.read_students();
     return 0;
 }
